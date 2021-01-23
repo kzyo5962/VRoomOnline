de22b1e48a89c3206a91daef24f9357f8df462f5
Quang Huy <kzyo5962@gmail.com>
2021-01-23 23:13:32 +0700

    CRUD for make

--- vroom/Controllers/MakeController.cs
+++ vroom/Controllers/MakeController.cs
@@ -3,24 +3,78 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using vroom.AppDBContext;
 using vroom.Models;
 
 namespace vroom.Controllers
 {
     public class MakeController : Controller
     {
-        [Route("make")]
-        [Route("make/bikes")]
-        public IActionResult Bikes()
+        private readonly VRoomDBContext _db;
+
+        public MakeController(VRoomDBContext db)
         {
-            Make make = new Make { Id = 1, Name = "Honda" };
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_db.Makes.ToList());
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Make make)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Add(make);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
             return View(make);
         }
 
-        [Route("make/bikes/{year:int:length(4)}/{month:int:range(1,13)}")]
-        public IActionResult ByMonthYear(int year, int month)
+        [HttpPost]
+        public IActionResult Delete(int id)
         {
-            return Content(year + "/" + month);
+            var make = _db.Makes.Find(id);
+            if(make == null)
+            {
+                return NotFound();
+            }
+            _db.Makes.Remove(make);
+            _db.SaveChanges();
+            return RedirectToAction(nameof(Index)); 
         }
+
+        public IActionResult Edit(int id)
+        {
+            var make = _db.Makes.Find(id);
+            if (make == null)
+            {
+                return NotFound();
+            }
+            
+            return View(make);
+        }
+        [HttpPost]
+        public IActionResult Edit(Make make)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Update(make);
+                _db.SaveChanges();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(make);
+        }
+       
+
+
+
     }
 }
