3121294429d1384568a0917a34ad72682ae7eed3
Quang Huy <kzyo5962@gmail.com>
2021-01-26 21:26:26 +0700

    using Extension Methods

--- vroom/Models/ModelViewModel.cs
+++ vroom/Models/ModelViewModel.cs
@@ -13,25 +13,6 @@ namespace vroom.Models
 
         public IEnumerable<Make> Makes { get; set; }
 
-        public IEnumerable<SelectListItem> CSelectListItem(IEnumerable<Make> Items) 
-        {
-            List<SelectListItem> MakeList = new List<SelectListItem>();
-            SelectListItem sli = new SelectListItem
-            {
-                Text = "-----Select-----",
-                Value = "0"
-            };
-            MakeList.Add(sli);
-            foreach(Make make in Items)
-            {
-                sli = new SelectListItem
-                {
-                    Text = make.Name,
-                    Value = make.Id.ToString()
-                };
-                MakeList.Add(sli);
-            }
-            return MakeList;
-        }
+        
     }
 }
